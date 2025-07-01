a8987c907d36d24aada6f92f027b2253f8f4c00b
SaNNa <alx.pav2106@gmail.com>
2025-07-01 18:41:43 +0300

    Added posting posts to the server

--- client/script.js
+++ client/script.js
@@ -1,3 +1,15 @@
-axios.get("http://localhost:3000/example").then(response => {
-    console.log(response.data);
+// axios.get("http://localhost:3000/example").then(response => {
+//     console.log(response.data);
+// });
+
+document.querySelector("#createPost").addEventListener("click", () => {
+    axios
+        .post("http://localhost:3000/posts", {
+            username: document.querySelector("#username-input").value,
+            postName: document.querySelector("#post-name-input").value,
+            postContent: document.querySelector("#post-content-input").value,
+        })
+        .then((response) => {
+            console.log(response.data);
+        });
 });
