1cf7bfe8367fa6108dbaa5b73d8fb1e3450d35a6
SaNNa <alx.pav2106@gmail.com>
2025-07-08 18:10:00 +0300

    Added posts end point

--- api/index.js
+++ api/index.js
@@ -1,26 +1,58 @@
 import express from "express";
 import cors from "cors";
 import { Client } from "pg";
- 
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
 const connection = new Client({
   user: "postgres",
   host: "localhost",
-  database: "FullStackDB",
-  password: "Pasha_superuser",
+  database: "Fullstack",
+  password: "SaNNa",
   port: 5432,
-});
- 
+})
+
 connection.connect();
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(morgan("tiny"))
+app.use(express.json())
 
 app.get("/example", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
+})
+
+app.post("/posts", (req, res) => {
+  console.log(req.body)
+  const username = req.body.username
+  const postName = req.body.postName
+  const postContent = req.body.postContent
+  connection.query(`INSERT INTO posts(username, postname, postcontent) VALUES ('${username}', '${postName}', '${postContent}')`, (err, res) => {
+    if (err) {
+      console.error(err.stack);
+    } else {
+      console.log("Data inserted");
+    }
+  })
+  res.send("Success")
+})
+
+app.get("/posts", (req, res) => {
+  connection.query("SELECT * FROM posts", (err, database_res) => {
+    if (err) {
+      console.error(err.stack);
+    } else {
+      console.log(database_res.rows)
+      res.send(database_res.rows)
+    }
+  })
 })
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
